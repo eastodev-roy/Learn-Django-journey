8b13971b18a72f532152d60c86a82e1a8e626965
eastodev288 <infoeato288@gmail.com>
2025-11-11 23:57:48 +0600

    chapter 2

---
+++ chapter2/01_variables.py
@@ -0,0 +1,6 @@
+a = 20 #integers
+b = "Eastodev" #Strings
+c = 25.052 #Folding point numbers
+b = 20 #integers
+print (a+b)
+
---
+++ chapter2/05_type.py
@@ -0,0 +1,12 @@
+c = 'hiiiiiiiii'
+t = type (c) #class <int>
+
+print (t)
+k = 20
+b = type (k) #class <int>
+
+print (b)
+o = 20.52
+n = type (o) #class <int>
+
+print (n)
